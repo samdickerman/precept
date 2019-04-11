b47dc57bd1fd9bf3270571160a299ab10b425902
Sam Dickerman <dickermansam@gmail.com>
2019-04-11 10:05:34 -0400

    Revert "update gdrive keys, etc"
    This reverts commit 786e3427cd0bb617b0aa1ab08f0f9ca5832fa084.

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@ export default {
   },
 
   gdrive: {
-    appId: "502455185443",
-    clientId: "502455185443-gr19tpj2gornm205e35tsfj7aog0jbc8.apps.googleusercontent.com",
-    apiKey: process.env.VUE_APP_API_KEY || "AIzaSyAI4ZQxYiw8ggmPrRB1HnDMVSiJaZb7gVw"
+    appId: "880472811488",
+    clientId: "880472811488-1hm06rum32dj0g28hkcedfb6h456ll4l.apps.googleusercontent.com",
+    apiKey: process.env.VUE_APP_API_KEY || "AIzaSyCT-dDWWmNJawfBf-Lot471GGtQrYk1fMQ"
   }
 
 }
